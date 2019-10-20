09256ad701ee8b92f08c5e675e957e9932bd466f
yizhaoxian <nilaiyaowoa@126.com>
2019-10-20 19:07:39 +0800

    增加转移日志

--- BatchTransfer/Index.cs
+++ BatchTransfer/Index.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using AssetsHelper.DBHelper;
 using AssetsHelper.Models;
+using NLog;
 
 namespace BatchTransfer
 {
@@ -18,8 +19,16 @@ namespace BatchTransfer
         CompanyHelper _companyHelper;
         AssetCategoryHelper _assetCategoryHelper;
         AssetHelper _assetHelper;
+        ILogger logger = NLog.LogManager.GetCurrentClassLogger();
 
+        /// <summary>
+        /// 需要新增的分类
+        /// </summary>
         IList<AssetCategory> assetCategoriesTemp = new List<AssetCategory>();
+        /// <summary>
+        /// 当前需要转移的部门和子部门
+        /// </summary>
+        IList<int> deptIdsTemp = new List<int>();
         /// <summary>
         /// 新城市的所有资产分类
         /// </summary>
@@ -35,10 +44,11 @@ namespace BatchTransfer
         }
 
         #region 事件
+
         private void Index_Load(object sender, EventArgs e)
         {
-            Console.WriteLine("----------- {0} 程序启动 -----------", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-            //城市列表
+            Console.WriteLine($"-----------{DateTime.Now} 资产转移执行日志 -----------");
+            //城市列表 
             var firstCompanyId = InitCompany();
             InitDept(firstCompanyId);
         }
@@ -70,7 +80,7 @@ namespace BatchTransfer
             int.TryParse(this.GoToCityList.SelectedValue.ToString(), out companyid);
             this.GoToCityId.Text = "城市Id:" + companyid;
         }
-         
+
         /// <summary>
         /// 确认转移
         /// </summary>
@@ -78,57 +88,94 @@ namespace BatchTransfer
         /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
+            #region 数据验证
+            var oldCompanyId = (int)SourceCityList.SelectedValue;
+            if (oldCompanyId < 1)
+            {
+                MessageBox.Show("请选择需要转移资产的城市", "系统提示");
+                return;
+            }
+            var newCompanyId = (int)GoToCityList.SelectedValue;
+            if (newCompanyId < 1)
+            {
+                MessageBox.Show("请选择接收资产的城市", "系统提示");
+                return;
+            }
+            var gotoDeptId = 0;
+            int.TryParse(GoToDeptId.Text, out gotoDeptId);
+            if (gotoDeptId == 0)
+            {
+                MessageBox.Show("请填写接收部门Id", "系统提示");
+                return;
+            }
+            var checkIds = GetCheckDept();
+            if (checkIds.Count < 1)
+            {
+                MessageBox.Show("请选择要转移的部门", "系统提示");
+                return;
+            }
+            var dept_goto = _deptHelper.GetById<Dept>(gotoDeptId);
+            if (dept_goto == null)
+            {
+                WriteLog($"接收部门Id 【{gotoDeptId}】 不存在");
+                return;
+            }
+
+            #endregion
+
             if (MessageBox.Show("确认转移勾选部门资产吗？", "系统提示", MessageBoxButtons.YesNo) == DialogResult.Yes)
             {
-                var gotoDeptId = 0;
-                int.TryParse(GoToDeptId.Text, out gotoDeptId);
-                if (gotoDeptId == 0)
-                {
-                    MessageBox.Show("请填写接收部门Id", "系统提示");
-                    return;
-                }
+                Console.WriteLine("-- {0} 资产开始转移", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                logger.Info("资产开始转移");
 
-                var checkIds = GetCheckDept();
-                if (checkIds.Count < 1)
-                {
-                    MessageBox.Show("请先选择要转移的部门", "系统提示");
-                    return;
-                }
-
-                Console.WriteLine("-- {0} 开始转移", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
-
-                var oldCompanyId = (int)SourceCityList.SelectedValue;
-                var newCompanyId = (int)GoToCityList.SelectedValue;
                 var updateDeptIds = string.Join(",", checkIds);
-
-                string tips = $"-- 将{this.SourceCityList.Text}【{oldCompanyId}】下的部门【{updateDeptIds}】以及资产、资产分类转移到{this.GoToCityList.Text}【{newCompanyId}】下";
-                Console.WriteLine(tips);
+                string transferinfo = $"将{this.SourceCityList.Text}【{oldCompanyId}】下的部门【{updateDeptIds}】以及资产、资产分类转移到{this.GoToCityList.Text}【{newCompanyId}】下";
+                WriteLog(transferinfo);
 
                 /* 
                  * 1、更新部门城市id, 并更新parentId\parentStr 
                  * 2、新增资产分类 , 并更新parentId\parentStr 
                  * 3、将原来的资产分类改为新增分类 
                  */
-                assetCategorieAllNewCompany = _assetCategoryHelper.LoadAll(newCompanyId);    //新城市的所有分类 
+                //新城市的所有分类 
+                assetCategorieAllNewCompany = _assetCategoryHelper.LoadAll(newCompanyId);
                 for (int i = 0; i < checkIds.Count; i++)
                 {
-                    var dept_goto = _deptHelper.GetById<Dept>(gotoDeptId);
-                    if (dept_goto != null)
+                    deptIdsTemp.Clear();
+                    /**************************** 转移部门 ****************************/
+                    // 1.1、查找所有子部门(含本身)
+                    var deptlist = _deptHelper.LoadAll(oldCompanyId, checkIds[i]);
+                    if (deptlist != null)
                     {
-                        /**************************** 转移部门 ****************************/
-                        // 1.1、查找所有子部门(含本身)
-                        var deptlist = _deptHelper.LoadAll(oldCompanyId, checkIds[i]);
+                        WriteLog($"开始转移部门【{checkIds[i]}】和所有子部门");
                         // 1.2、更新所有子部门(含本身)  
                         UpdateSubDeptCompany(checkIds[i], deptlist, oldCompanyId, newCompanyId, dept_goto.id, dept_goto.parentStr);
                         /*************************** 新增资产分类并更新资产分类id *************/
-                        var depts = "1295,4511,1296";
-                        var categroys_source = _assetCategoryHelper.LoadAll(depts);         //转移资产的所有分类 
-                        for (int c = 0; c < categroys_source.Count; c++)
+                        var depts = string.Join(",", deptIdsTemp);
+                        WriteLog($"部门【{checkIds[i]}】和所有子部门id，【{depts}】");
+                        WriteLog($"开始复制资产分类");
+                        //需要转移资产的所有分类 
+                        var categroys_source = _assetCategoryHelper.LoadAll(depts);
+                        if (categroys_source != null && categroys_source.Count > 0)
                         {
-                            CopyCategory(categroys_source[c], newCompanyId, depts, categroys_source[c].id);
+                            for (int c = 0; c < categroys_source.Count; c++)
+                            {
+                                //新增分类，并将资产分配到新分类
+                                CopyCategory(categroys_source[c], newCompanyId, depts, categroys_source[c].id);
+                            }
                         }
+                        else
+                        {
+                            WriteLog($"当前转移部门没有需要复制的资产分类");
+                        }
+                        WriteLog($"部门【{checkIds[i]}】和所有子部门【{depts}】转移完成");
+                    }
+                    else
+                    {
+                        WriteLog($"转移部门【{checkIds[i]}】找不到");
                     }
                 }
+                logger.Info("--------------------------------------------------------------------------------------");
             }
         }
 
@@ -138,14 +185,15 @@ namespace BatchTransfer
         /// 更新所有部门的新城市id,parentid,parentstr
         /// </summary>
         /// <param name="parentid"></param>
-        /// <param name="deptDatas"></param>
+        /// <param name="deptDatas">所有子部门(含本身)</param>
         private void UpdateSubDeptCompany(int sourceParentId, IList<DeptData> deptDatas, int oldCompanyId, int newCompanyId, int newParentId, string newParentStr)
         {
             if (deptDatas == null) return;
             //转移父部门自身城市
             var newpstr = newParentStr + "," + sourceParentId;
             _deptHelper.UpdateCompanyId(oldCompanyId, newCompanyId, sourceParentId, newParentId, newpstr);
-
+            WriteLog($"Update Dept newCompanyId={newCompanyId},newParentId={newParentId},newpstr={newpstr} where Id={sourceParentId}");
+            deptIdsTemp.Add(sourceParentId);
             var sublist = deptDatas.Where(l => l.parentId == sourceParentId).ToList();
             if (sublist != null && sublist.Count > 0)
             {
@@ -273,12 +321,14 @@ namespace BatchTransfer
             {
                 if (assetCategorySource.id == originalCategoryId)//存在相同分类，不用复制新分类，直接更新资产分类id
                 {
+                    WriteLog($"资产分类【{existsCategory.name},{existsCategory.id}】已存在，并且此分类为需要更新资产的分类，直接更新资产的分类id");
                     _assetHelper.UpdateCategoryId(existsCategory.id, assetCategorySource.id, assetusingDeptIds);
                 }
                 else
                 {
                     var parentStr = existsCategory.parentStr;
                     var newCategoryId = existsCategory.id;
+                    WriteLog($"资产分类【{existsCategory.name},{existsCategory.id}】已存在，开始复制子分类");
                     if (assetCategoriesTemp.Count > 0)
                     {
                         for (int i = assetCategoriesTemp.Count - 1; i >= 0; i--)
@@ -325,13 +375,16 @@ namespace BatchTransfer
         {
             //1、复制到新城市，最后返回新分类id
             var newCategoryId = _assetCategoryHelper.InsertCopy(assetCategorySource.id, newCompanyId);
+            WriteLog($"1、复制到新城市，返回新分类【{assetCategorySource.name},{newCategoryId}】");
             //2、更新分类parentId，parentStr 
             parentStr += "," + newCategoryId;
             _assetCategoryHelper.UpdateParent(newCategoryId, parentid, parentStr);
+            WriteLog($"2、更新新分类【parentid={parentid},parentStr={parentStr}】");
             //3、更新资产的分类Id
             if (assetCategorySource.id == originalCategoryId)
             {
-                _assetHelper.UpdateCategoryId(newCategoryId, assetCategorySource.id, assetusingDeptIds);
+                var assetRows = _assetHelper.UpdateCategoryId(newCategoryId, assetCategorySource.id, assetusingDeptIds);
+                WriteLog($"3、更新资产的新分类【id={newCategoryId}】,使用部门【usingDeptId={assetusingDeptIds}】,资产数量【{assetRows}】");
             }
             //将新添加分类放入到临时表里，减少数据库查询
             assetCategorieAllNewCompany.Add(new AssetCategory
@@ -349,5 +402,12 @@ namespace BatchTransfer
         }
 
         #endregion
+
+
+        private void WriteLog(string msg)
+        {
+            Console.WriteLine(msg);
+            logger.Info(msg);
+        }
     }
 }
